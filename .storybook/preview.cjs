5d2331efa7b39a1e9f89f88628d79b2cb10b6a34
Alberto Araujo <albertoaraujo.dev@gmail.com>
2022-12-04 16:17:31 -0300

    primeiro componente Text

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,8 @@
-import { themes } from "@storybook/theming";
+import { themes } from '@storybook/theming';
+import '../src/styles/global.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./styles/global.css";
+import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1 className="font-bold tex-5xl text-black">Hello World</h1>
+      <h1 className="font-bold text-2xl text-black">Hello World</h1>
 
       <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-black hover:bg-cyan-300">
         Enviar
